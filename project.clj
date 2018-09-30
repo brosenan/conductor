d65ef6f4f24a3ff0031230d2dcbc57f824dff48c
Boaz Rosenan <brosenan@gmail.com>
2018-09-30 16:26:35 +0300

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brosenan/conductor "0.2.0"
+(defproject brosenan/conductor "0.2.1-SNAPSHOT"
   :description "A lambda-kube recipe for installing Netflix Conductor"
   :url "https://github.com/brosenan/conductor"
   :license {:name "Eclipse Public License"
